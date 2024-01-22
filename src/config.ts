1d6bd5d1932967c30e76be48caa3cb2d2cd8e70b
Ertan Muslic <ertanmuslic@gmail.com>
2024-01-22 02:50:24 +0100

    .

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://ertan.pythonanywhere.com/",
+  baseURL: "https://ertan.pythonanywhere.com/",
   // You can add other configuration options here
 });
 
